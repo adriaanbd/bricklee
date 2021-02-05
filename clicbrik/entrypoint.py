9bb664b2db073e4c2fd23d9b758b1a509b03ff62
Adriaan Beiertz <adriaanbd@gmail.com>
2021-02-05 00:38:35 -0500

    working greeting cli

---
+++ clicbrik/entrypoint.py
@@ -0,0 +1,17 @@
+import argparse
+import sys
+
+def greet(name):
+    if name:
+        return f'Hello {name}.'
+    return f'Hello Panama City, Panama.'
+
+def main(args=None):
+    if not args:
+        args = sys.argv[1:]
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('hello', type=str, help='a generic greeting')
+    parser.add_argument('--name', help='a customized greeting')
+    args = parser.parse_args(args)
+    greeting = greet(args.name)
+    print(greeting)
