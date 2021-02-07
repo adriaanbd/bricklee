f85264b5a419ac80573b06d8fd48af01cc099fd2
Adriaan Beiertz <adriaanbd@gmail.com>
2021-02-06 22:35:03 -0500

    added tests for customized greeting

--- tests/features/cli.feature
+++ tests/features/cli.feature
@@ -1,7 +1,7 @@
 Feature: Greeting
     Scenario: Generic Greeting
-        Given the user runs the clicbrik hello command with no parameters: "bricklee hello"
-        Then the CLI returns "Hello Panama City, Panama."
+        Given the user runs the bricklee hello command with no parameters
+        Then the CLI returns a generic response
     Scenario: Customized Greeting
-        Given the user runs the clicbrik hello command with the --name flag and 1 parameter: "bricklee hello --name Adriaan"
-        Then the CLI returns "Hello Adriaan."
+        Given the user runs the bricklee hello command with the --name flag and 1 parameter
+        Then the CLI returns a customized response
--- tests/features/steps/cli.py
+++ tests/features/steps/cli.py
@@ -1,13 +1,24 @@
 from behave import *
 import subprocess
 
-@given('the user runs the clicbrik hello command with no parameters: {text}')
-def step_impl(context, text):
+@given('the user runs the bricklee hello command with no parameters')
+def step_impl(context):
     args = ['poetry', 'run', 'bricklee', 'hello']
     context.response = subprocess.run(args, capture_output=True)
 
-@then('the CLI returns {text}')
-def step_impl(context, text):
+@then('the CLI returns a generic response')
+def step_impl(context):
     bytes_output = context.response.stdout
     expected = "Hello Panama City, Panama."
+    assert expected in bytes_output.decode()
+
+@given('the user runs the bricklee hello command with the --name flag and 1 parameter')
+def step_impl(context):
+    args = ['poetry', 'run', 'bricklee', 'hello', '--name', 'Adriaan']
+    context.response = subprocess.run(args, capture_output=True)
+
+@then('the CLI returns a customized response')
+def step_impl(context):
+    bytes_output = context.response.stdout
+    expected = "Hello Adriaan."
     assert expected in bytes_output.decode()
